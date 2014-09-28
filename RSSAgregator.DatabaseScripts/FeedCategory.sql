dfa5561d8853dd26046617db45b3145503810921
Aurelien Souchet <souchet.aurelien@gmail.com>
2014-09-28 17:44:52 +0200

    Creation de la BDD, et debut de dataContext (tables User / FeedCategory / FeedSource)

---
+++ RSSAgregator.DatabaseScripts/FeedCategory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FeedCategory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [Name] VARCHAR(50) NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [Public] TINYINT NOT NULL, 
+    [CreationDate] DATETIME NOT NULL
+)
