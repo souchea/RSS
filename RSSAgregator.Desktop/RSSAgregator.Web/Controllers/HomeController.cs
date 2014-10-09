fa17cb3049fe73757b228319531127383a2333ac
di-bel_f <flav.dibello@gmail.com>
2014-10-09 19:12:24 +0200

    add subview for categories items channels

--- RSSAgregator.Desktop/RSSAgregator.Web/Controllers/HomeController.cs
+++ RSSAgregator.Desktop/RSSAgregator.Web/Controllers/HomeController.cs
@@ -15,12 +15,39 @@ namespace RSSAgregator.Web.Controllers
             return View("Index");
         }
 
-        // GET: json categories
+        // GET: categories
         [Route("categories")]
         public ActionResult GetCategories()
         {
-            return Json(new { foo = "bar", baz = "Blech" }, JsonRequestBehavior.AllowGet );
+            //return Json(new { foo = "bar", baz = "Blech" }, JsonRequestBehavior.AllowGet );
+
+            return PartialView("Categories");
+
         }
+
+        // GET: channels
+        [Route("{category}/channels")]
+        public ActionResult GetChannels(string category)
+        {
+            //return Json(new { foo = category, baz = "Blech" }, JsonRequestBehavior.AllowGet );
+
+
+            return PartialView("Channels");
+
+        }
+
+
+        // GET: items
+        [Route("{channel}/items")]
+        public ActionResult GetItems(string channel)
+        {
+            //return Json(new { foo = "bar", baz = "Blech" }, JsonRequestBehavior.AllowGet );
+
+            return PartialView("Items");
+
+        }
+
+
 
     }
 
