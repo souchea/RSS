fd01ab137b93cde4349828493bf90edb87f36a9f
madks13 <nikola.tomasevic@epitech.eu>
2014-11-29 15:17:53 +0100

    StorageManager mis a jour

--- RSSAgregator.Desktop/Manager/StorageManager.cs
+++ RSSAgregator.Desktop/Manager/StorageManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
 using RSSAgregator.Shared.Common;
 using RSSAgregator.Shared.Model;
 
@@ -11,27 +13,59 @@ namespace RSSAgregator.Desktop.Manager
     public class StorageManager : IStorageManager
     {
         #region IStorageManager Members
-
-        public async  Task<List<Shared.Model.CategoryDTO>> GetStoredCategories()
+        class StorageManager : IStorageManager
         {
-            return new List<CategoryDTO>();
-        }
 
-        public async Task<List<Shared.Model.SourceDTO>> GetStoredSources()
-        {
-           return new List<SourceDTO>();
-        }
+            public async Task<List<CategoryDTO>> GetStoredCategories()
+            {
+                try
+                {
+                    String path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "RSSAgregator", "categories.xml");
+                    FileStream stream = File.OpenRead(path);
 
-        public void StoreCategories(List<Shared.Model.CategoryDTO> toStore)
-        {
-            
-        }
+                    var serializer = new DataContractSerializer(typeof(List<CategoryDTO>));
 
-        public void StoreSources(List<Shared.Model.SourceDTO> toStore)
-        {
-           ;
-        }
+                    return serializer.ReadObject(stream) as List<CategoryDTO>;
+                }
+                catch (Exception)
+                {
+                    return new List<CategoryDTO>();
+                }
+            }
 
+            public async Task<List<SourceDTO>> GetStoredSources()
+            {
+                try
+                {
+                    String path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "RSSAgregator", "sources.xml");
+                    FileStream stream = File.OpenRead(path);
+
+                    var serializer = new DataContractSerializer(typeof(List<CategoryDTO>));
+
+                    return serializer.ReadObject(stream) as List<SourceDTO>;
+                }
+                catch (Exception)
+                {
+                    return new List<SourceDTO>();
+                }
+            }
+
+            public void StoreCategories(List<CategoryDTO> toStore)
+            {
+                String path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "RSSAgregator", "categories.xml");
+                FileStream stream = File.Open(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(List<CategoryDTO>));
+                serializer.WriteObject(stream, toStore);
+            }
+
+            public void StoreSources(List<SourceDTO> toStore)
+            {
+                String path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "RSSAgregator", "sources.xml");
+                FileStream stream = File.Open(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(List<SourceDTO>));
+                serializer.WriteObject(stream, toStore);
+            }
+        }
         #endregion
     }
 }
